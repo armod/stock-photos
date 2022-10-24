b595a1ea0090fa384727a2f1871317ddf5adfe5f
armod <armods@gmail.com>
2022-10-24 13:04:39 +0200

    photos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { FaSearch } from 'react-icons/fa'
+import Photo from './Photo'
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const mainUrl = `https://api.unsplash.com/photos/`
+const searchUrl = `https://api.unsplash.com/search/photos/`
+
+//52_0Yfy7XwBy_4ee455NH_FDwJvMnVUljPOvcNgWSYc
+
+//e9r1PQ027-Qcz0qVicqCUbliFuCUkiZYsyuw8XA6m2k
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className='App'></div>
 }
 
-export default App;
+export default App
