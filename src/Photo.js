8b7f823018f264237cb5602094ad4944e0984786
armod <armods@gmail.com>
2022-10-24 15:38:11 +0200

    photos

--- src/Photo.js
+++ src/Photo.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
-const Photo = () => {
-  return <div>Photo</div>
+const Photo = ({
+  urls: { regular },
+  alt_decription,
+  likes,
+  user: {
+    name,
+    portfolio_url,
+    profile_image: { medium },
+  },
+}) => {
+  return (
+    <article className='photo'>
+      <img src={regular} alt={alt_decription} />
+      <div className='photo-info'>
+        <div>
+          <h4>{name}</h4>
+          <p>{likes} likes</p>
+        </div>
+        <a href={portfolio_url}>
+          <img src={medium} alt='' className='user-img' />
+        </a>
+      </div>
+    </article>
+  )
 }
 
 export default Photo
